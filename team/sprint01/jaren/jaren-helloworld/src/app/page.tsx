f75d68e45c672d1a5e90b612ed9c593d79d99123
Jaren Lowe <jaren@ucsb.edu>
2025-01-15 14:46:05 -0800

    created helloworld app

---
+++ team/sprint01/jaren/jaren-helloworld/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Welcome to GauchoGraduate App</h1>
+    </div>
+  );
+}
