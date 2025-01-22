c8894a34227bda5b59a55fbc8d117727681ab4b6
Chiran Arumugam <carumugam@ucsb.edu>
2025-01-22 15:11:37 -0800

    homepage added

---
+++ gauchograduate/src/app/homepage/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="flex flex-row">
+          
+        </div>
+    </div>
+  );
+}
