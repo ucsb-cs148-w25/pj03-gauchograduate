e87c31d8533fe68a53359f7cd9d358672473a6b9
Chiran Arumugam <carumugam@ucsb.edu>
2025-01-22 19:56:53 -0800

    updated the homepage to include the three different sections

--- gauchograduate/src/app/homepage/page.tsx
+++ gauchograduate/src/app/homepage/page.tsx
@@ -2,10 +2,30 @@
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <div className="flex flex-row">
-          
+    <div className="min-h-screen flex flex-col">
+      {/* Nav Bar */}
+      <nav className="p-5 bg-[var(--background)] border-b border-gray-300">
+        <h1 className="text-2xl font-bold">GauchoGraduate</h1>
+      </nav>
+
+
+      <div className="flex flex-1">
+        {/* Course Catalog */}
+        <div className="w-1/4 bg-[var(--off-white)]  p-4">
+          <h2 className="text-xl font-semibold">Course Catalog</h2>
         </div>
+
+        {/* 4-year calendar */}
+        <div className="w-2/4 bg-white p-4 rounded-md shadow">
+          <h2 className="text-xl font-semibold">Four Year Calendar</h2>
+        </div>
+
+        {/* Graduation Progress */}
+        <div className="w-1/4 bg-[var(--off-white)] p-4">
+          <h2 className="text-xl font-semibold">Progress Tracker</h2>
+        </div>
+      </div>
     </div>
   );
 }
+
