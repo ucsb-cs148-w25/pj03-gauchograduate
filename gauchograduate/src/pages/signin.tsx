f39962f5fe38f9d0c248bbf04808fced0280ba55
Selena Zeng <selenazeng@ucsb.edu>
2025-02-18 20:58:47 -0800

    sign-in page design and added logo

--- gauchograduate/src/pages/signin.tsx
+++ gauchograduate/src/pages/signin.tsx
@@ -1,25 +1,25 @@
 import { signIn } from 'next-auth/react';
 import Navbar from '../app/components/Navbar';
 import GoogleLogo from '../app/components/icons/GoogleLogo';
+import Image from 'next/image';
 
 export default function SignIn() {
     return (
-        <div className="min-h-screen flex flex-col bg-gray-100">
-            <Navbar />
+        <div className="min-h-screen flex flex-col bg-gradient-to-br from-white to bg-[var(--pale-blue)]">
             
             <div className="flex-1 flex items-center justify-center">
-                <div className="bg-white p-6 rounded-2xl shadow-lg flex flex-row items-center gap-12 w-full max-w-2xl">
+                <div className="bg-white p-6 rounded-2xl shadow-lg flex flex-row items-center gap-12 w-full max-w-3xl">
                     {/* Colored Grid */}
                     <div className="grid grid-cols-2 gap-2">
-                        <div className="w-32 h-32 bg-[var(--pale-orange)] rounded-lg"></div>
-                        <div className="w-32 h-32 bg-[var(--pale-pink)] rounded-lg"></div>
-                        <div className="w-32 h-32 bg-[var(--pale-green)] rounded-lg"></div>
-                        <div className="w-32 h-32 bg-[var(--pale-blue)] rounded-lg"></div>
+                        <div className="w-48 h-48 bg-[var(--pale-orange)] rounded-lg"></div>
+                        <div className="w-48 h-48 bg-[var(--pale-pink)] rounded-lg"></div>
+                        <div className="w-48 h-48 bg-[var(--pale-green)] rounded-lg"></div>
+                        <div className="w-48 h-48 bg-[var(--pale-blue)] rounded-lg"></div>
                     </div>
 
                     {/* Sign-in Section */}
                     <div className="text-left ml-6">
-                        <h1 className="text-3xl font-bold mb-2">Welcome!</h1>
+                    <Image src="/logo1.png" alt="Gaucho Graduate Logo" width={180} height={60} />
                         <p className="text-gray-600 mb-6">Please sign in to continue.</p>
                         <button 
                             onClick={() => signIn('google', { callbackUrl: '/'})}
