c3a1c92c058754704cb2f4221cda3db1350b971a
Va1a <54305812+Va1a@users.noreply.github.com>
2025-02-20 20:14:46 -0800

    Use chromium for tests only.

--- gauchograduate/playwright.config.ts
+++ gauchograduate/playwright.config.ts
@@ -39,15 +39,16 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    /* We only need Chromium */
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
