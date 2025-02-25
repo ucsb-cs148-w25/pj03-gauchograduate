414ff51f1b9b77524de351c309e7aca8925b1ba7
Va1a <54305812+Va1a@users.noreply.github.com>
2025-02-24 17:18:18 -0800

    Add favicons

--- gauchograduate/src/pages/_document.tsx
+++ gauchograduate/src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
